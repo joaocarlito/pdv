897559eead0516e1c70989ffe3e9e1d9efcd0cfd
joaocarlito <joaocarlito@outlook.com.br>
2018-09-26 20:41:10 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Frente de Caixa</title>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        
+    </head>
+    <body>
+        
+        <div class="container">
+            <div class="row">
+            
+                
+                
+            </div>
+        </div>
+        
+    </body>
+</html>
